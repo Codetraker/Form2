7420a78f25339aa7f636456c89d8b85a2689c250
Codetraker <swarajbibhuprasad@gmail.com>
2023-08-29 17:50:54 +0530

    add filter part

--- form2.js
+++ form2.js
@@ -1,5 +1,6 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 //-----------------------------------------------------------
     let newbtn = document.querySelectorAll('.list-group-item');
 
@@ -65,3 +66,20 @@ function removeItem(e){
     }
   }
 }
+
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get list
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
